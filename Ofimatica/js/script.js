bcb59487d006284351694ef4888b6fb3301c8c06
John Muñoz <jgmunozc@outlook.com>
2024-01-19 08:34:39 -0500

    Update script.js

--- Ofimatica/js/script.js
+++ Ofimatica/js/script.js
@@ -37,5 +37,22 @@ $(document).ready(function(){
                   $("#info>#con>#ej3>p").append("<h5> Ep = 1,008 J </h5><br>");
                                                          
                                 		});
+
+
+		var coll = document.getElementsByClassName("collapsible");
+		var i;
+		
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var content = this.nextElementSibling;
+		    if (content.style.display === "block") {
+		      content.style.display = "none";
+		    } else {
+		      content.style.display = "block";
+		    }
+		  });
+		}
+	
   
 	});
